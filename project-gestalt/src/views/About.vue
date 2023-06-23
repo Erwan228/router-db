5524972d91844c3e00519cc6f800ab5a5d43be5f
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-23 11:07:09 +0200

    Update About.vue

--- project-gestalt/src/views/About.vue
+++ project-gestalt/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Project Gestalt</h1>
+    <p>Project Gestalt was a desperate initiative aimed at preserving humanity in the face of a devastating disease called the Black Scrawl. It involved separating human souls from their bodies and transferring them into artificial beings called Gestalts. The empty physical bodies left behind became Replicants, designed to house the Gestalts' data. The goal was for the Replicants and Gestalts to exist in a symbiotic relationship, with the Replicants acting as vessels for the Gestalts' emotions and memories. However, complications arose as the Gestalts developed their own consciousness, leading to unforeseen consequences and conflicts. The shades, monstrous beings born from incomplete Gestalt transformations and influenced by external forces, became a threat to the surviving humans and Replicants. Project Gestalt ultimately faced numerous challenges and moral dilemmas, exploring themes of identity, sacrifice, and the consequences of tampering with the nature of humanity.</p>
   </div>
 </template>
