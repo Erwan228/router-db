afeffc5276c0ee3a7261a61fcf19fea8cfa79657
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-23 10:36:32 +0200

    Update Home.vue

--- project-gestalt/src/views/Home.vue
+++ project-gestalt/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="Home">
-    
+    <h1>Welcome to Project Gestalt</h1>
+    <h2>We have documented what little information we can share with you about Project Gestalt</h2>
   </div>
 </template>
 
