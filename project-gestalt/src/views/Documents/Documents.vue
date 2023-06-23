efd2ad4d6b9e6d560daec96af3bc79f7f7a9a0b9
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-23 14:33:26 +0200

    Update Documents.vue

--- project-gestalt/src/views/Documents/Documents.vue
+++ project-gestalt/src/views/Documents/Documents.vue
@@ -1,7 +1,37 @@
 <template>
     <h1>Detailed documents</h1>
     <h2>Specimens</h2>
+        <div v-if="specimens.length" class="specimens">
+            <div v-for="specimen in specimens" :key="specimen.id" class="specimen">
+                <router-link :to="{ name: 'Specimens', params: {id: specimen.id} }">
+                    <h3>{{ specimen.name }}</h3>
+                </router-link>
+            </div>
+        </div>
 </template>
+
+<script>
+export default{
+    data() {
+        return{
+            specimens: [],
+            weapons: []
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/specimens')
+        .then(res => res.json())
+        .then(data => this.specimens = data)
+        .catch(err => console.log(err.message))
+
+        fetch('http://localhost:3000/weapons')
+        .then(res => res.json())
+        .then(data => this.weapons = data)
+        .catch(err => console.log(err.message))
+    }
+}
+
+</script>
 
 <style scoped>
 h2 {
